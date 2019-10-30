847a43e95effa932005802c37c662c9f58fe0adc
Chase J. Clark <chase_clark07@hotmail.com>
2019-10-30 16:36:04 -0400

    default to swipe layout and add datepicker dropdown

--- js/init.js
+++ js/init.js
@@ -1,7 +1,16 @@
 (function($){
   $(function(){
-
     M.AutoInit();
+
+    // Date Picker
+    var options = {
+      defaultDate: new Date(),
+      setDefaultDate: true
+    };
+    var elems = document.querySelector('#date');
+    var instance = M.Datepicker.init(elems, options);
+    // current date stored in instance.toString() - need to add event listener for when date changes
+    console.log(instance.toString());
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
